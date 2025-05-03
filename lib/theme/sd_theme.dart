e74f12eb5c8358394a063042a7fac44a6eccb2ba
Dam Hong Duc <hongduc001h@gmail.com>
2025-05-03 15:54:31 +0700

    feat: wip

---
+++ lib/theme/sd_theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class SdTheme {
+  static ThemeData light({
+    String? fontFamily,
+    List<ThemeExtension<dynamic>>? extensions,
+  }) {
+    return ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.light,
+      fontFamily: fontFamily,
+      extensions: [if (extensions != null) ...extensions],
+    );
+  }
+
+  static ThemeData dark({
+    String? fontFamily,
+    List<ThemeExtension<dynamic>>? extensions,
+  }) {
+    return ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.dark,
+      fontFamily: fontFamily,
+      extensions: [if (extensions != null) ...extensions],
+    );
+  }
+}
