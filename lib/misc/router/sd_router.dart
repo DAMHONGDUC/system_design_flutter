18f4ca8a84e7fb06160511839ad40b1d27757ae9
Dam Hong Duc <hongduc001h@gmail.com>
2025-06-07 23:30:41 +0700

    feat: wip

--- lib/misc/router/sd_router.dart
+++ lib/misc/router/sd_router.dart
@@ -1,17 +1,14 @@
 import 'package:equatable/equatable.dart';
 
-/// Represents a defined route in the application.
 class SdRouter extends Equatable {
   const SdRouter({
     required this.name,
-    @Deprecated(
-      'Use fullPath instead. This property will be removed in future versions.',
-    )
     required this.path,
     required this.fullPath,
   });
 
   /// The path segment for this route (e.g., 'login', 'home').
+
   final String path;
 
   /// A unique name for this route (e.g., 'loginRoute', 'homeRoute').
