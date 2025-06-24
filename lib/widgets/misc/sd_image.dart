95f6f3bf49cd5411a84371871c05071a2085e243
Dam Hong Duc <hongduc001h@gmail.com>
2025-06-25 00:12:45 +0700

    feat: update sd image

--- lib/widgets/misc/sd_image.dart
+++ lib/widgets/misc/sd_image.dart
@@ -1,16 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:system_design_flutter/resources/resources.dart';
+import 'package:system_design_flutter/resources/sd_colors.dart';
+import 'package:system_design_flutter/widgets/icon/sd_icon.dart';
 
 class SdImage extends StatelessWidget {
-  const SdImage({super.key, required this.imgPath, this.width, this.height});
-  final String imgPath;
+  final String imagePath;
   final double? width;
   final double? height;
+  final BoxFit? boxFit;
+
+  const SdImage({
+    Key? key,
+    required this.imagePath,
+    this.width,
+    this.height,
+    this.boxFit = BoxFit.cover,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final size = SdSpacing.s24;
-
-    return Image.asset(imgPath, width: width ?? size, height: height ?? size);
+    return Image.asset(
+      imagePath,
+      width: width,
+      height: height,
+      fit: boxFit,
+      errorBuilder: (context, error, stackTrace) {
+        return Container(
+          width: width,
+          height: height,
+          decoration: BoxDecoration(
+            color: Colors.grey[300],
+            border: Border.all(color: Colors.grey),
+          ),
+          child: SdIcon(iconData: Icons.broken_image, color: SdColors.grey),
+        );
+      },
+    );
   }
 }
