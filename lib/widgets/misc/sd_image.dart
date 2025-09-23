d35feae3566beb61be1b6765fc9902a6043ae272
Dam Hong Duc <hongduc001h@gmail.com>
2025-09-23 22:44:26 +0700

    feat: update SdImage to use local img & remote img

--- lib/widgets/misc/sd_image.dart
+++ lib/widgets/misc/sd_image.dart
@@ -1,45 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:system_design_flutter/resources/sd_colors.dart';
+import 'package:system_design_flutter/utils/sd_extensions.dart';
 import 'package:system_design_flutter/widgets/icon/sd_icon.dart';
 
 class SdImage extends StatelessWidget {
-  final String imagePath;
   final double? width;
   final double? height;
   final BoxFit? boxFit;
   final double? borderRadiusValue;
+  final String imagePath;
 
   const SdImage({
     Key? key,
-    required this.imagePath,
     this.width,
     this.height,
     this.boxFit = BoxFit.cover,
     this.borderRadiusValue,
+    required this.imagePath,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(borderRadiusValue ?? 0),
-      child: Image.asset(
-        imagePath,
-        width: width,
-        height: height,
-        fit: boxFit,
-        errorBuilder: (context, error, stackTrace) {
-          return Container(
-            width: width,
-            height: height,
-            decoration: BoxDecoration(
-              color: Colors.grey[300],
-              border: Border.all(color: Colors.grey),
-              borderRadius: BorderRadius.circular(borderRadiusValue ?? 0),
-            ),
-            child: SdIcon(iconData: Icons.broken_image, color: SdColors.grey),
-          );
-        },
-      ),
-    );
+    final isRemoteImg = imagePath.startsWith('http');
+
+    if (!isRemoteImg) {
+      return ClipRRect(
+        borderRadius: BorderRadius.circular(borderRadiusValue ?? 0),
+        child: Image.asset(
+          imagePath!,
+          width: width,
+          height: height,
+          fit: boxFit,
+          errorBuilder: (context, error, stackTrace) {
+            return Container(
+              width: width,
+              height: height,
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(borderRadiusValue ?? 0),
+              ),
+              child: SdIcon(iconData: Icons.broken_image, color: SdColors.grey),
+            );
+          },
+        ),
+      );
+    } else if (isRemoteImg) {
+      // TODO: handle UI for image URL
+    }
+    return SizedBox.shrink();
   }
 }
