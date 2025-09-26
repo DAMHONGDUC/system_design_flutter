dcb88b8c30a58de743f8b05e20e396b86c373e39
Dam Hong Duc <hongduc001h@gmail.com>
2025-09-26 17:51:03 +0700

    feat: add getContrastColor & update getColorFromHex

--- lib/utils/sd_color_helper.dart
+++ lib/utils/sd_color_helper.dart
@@ -1,13 +1,23 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
 
 class SdColorHelper {
   static Color getColorFromHex(String hexColor) {
-    hexColor = hexColor.replaceAll('#', '');
+    try {
+      hexColor = hexColor.replaceAll('#', '');
 
-    if (hexColor.length == 6) {
-      hexColor = 'FF$hexColor';
+      if (hexColor.length == 6) {
+        hexColor = 'FF$hexColor';
+      }
+
+      return Color(int.parse(hexColor, radix: 16));
+    } catch (err) {
+      return Colors.black;
     }
+  }
 
-    return Color(int.parse(hexColor, radix: 16));
+  static Color getContrastColor(Color background, {double threshold = 0.5}) {
+    // Flutter's computeLuminance gives a perceptual brightness (0.0 - 1.0)
+    final luminance = background.computeLuminance();
+    return luminance > threshold ? Colors.black : Colors.white;
   }
 }
